ebc4f2eee80c480d66882402e1e38ca3ef02fa39
Santiago Nunez-Corrales <snunezcr@gmail.com>
2026-07-11 19:28:51 -0400

    Revert "Implement preparatory PR for #1150 v3: retire sentinel lambdas (Action/Match/Unsafe)"
    This reverts commit ecc4a0c05a17468654484116331263148b93627f.

--- crates/paideia-as/tests/build_emit.rs
+++ crates/paideia-as/tests/build_emit.rs
@@ -47,12 +47,10 @@ mod build_emit {
     pub mod pa7c_plt32_witness;
     pub mod pa7c_symbol_export;
     pub mod pa7c_unsafe_body;
-    pub mod pa8_action_body_st_value;
     pub mod pa8_call_st_value;
     pub mod pa8_cross_module_call;
     pub mod pa8_m1_001b_lambda_params;
     pub mod pa8_m3_004;
-    pub mod pa8_match_body_st_value;
     pub mod pa8_mixed_shapes;
     pub mod pa8_add_imm_out_of_range;
     pub mod pa8_shl_imm_out_of_range;
